baced9c203a527465a8a562275c3d2298d74ae3e
mayurshah530 <mayur.shah@gmail.com>
2023-10-29 07:47:23 -0700

    Test - add BasicOmniOpMode_Linear

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/FirstVisionProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/FirstVisionProcessor.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.processors;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 
 
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
@@ -15,6 +17,10 @@ import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 public class FirstVisionProcessor implements VisionProcessor {
+    public Rect rectLeft = new Rect(8, 120, 100, 200);
+    public Rect rectMiddle = new Rect(160, 70, 200, 100);
+    public Rect rectRight = new Rect(500, 120, 100, 200);
+    Selected selection = Selected.NONE;
     ScoringElementLocation selection = ScoringElementLocation.UNKNOWN;
 
     // Define lower and upper HSV values for the red and blue color ranges
@@ -23,6 +29,8 @@ public class FirstVisionProcessor implements VisionProcessor {
     public Scalar lowerBlue = new Scalar(100, 100, 100);
     public Scalar upperBlue = new Scalar(140, 255, 255);
 
+    Mat submat = new Mat();
+    Mat hsvMat = new Mat();
     private Mat hsvImage;
     private Mat mask;
 
@@ -42,6 +50,44 @@ public class FirstVisionProcessor implements VisionProcessor {
     }
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
+        Imgproc.cvtColor(frame, hsvMat, Imgproc.COLOR_RGB2HSV);
+        double satRectLeft = getAvgSaturation(hsvMat, rectLeft);
+        double satRectMiddle = getAvgSaturation(hsvMat, rectMiddle);
+        double satRectRight = getAvgSaturation(hsvMat, rectRight);
+
+        if ((satRectLeft > satRectMiddle) && (satRectLeft > satRectRight)) {
+            return Selected.LEFT;
+            } else if ((satRectMiddle > satRectLeft) && (satRectMiddle > satRectRight)) {
+            return Selected.MIDDLE;
+            }
+        return Selected.RIGHT;
+        }
+
+        protected double getAvgSaturation(Mat input, Rect rect) {
+        submat = input.submat(rect);
+        Scalar color = Core.mean(submat);
+        return color.val[1];
+        }
+
+    private android.graphics.Rect makeGraphicsRect(Rect rect, float  scaleBmpPxToCanvasPx) {
+        int left = Math.round(rect.x * scaleBmpPxToCanvasPx);
+        int top = Math.round(rect.y * scaleBmpPxToCanvasPx);
+        int right = left + Math.round(rect.width * scaleBmpPxToCanvasPx); int bottom = top + Math.round(rect.height * scaleBmpPxToCanvasPx);
+        return new android.graphics.Rect(left, top, right, bottom); }
+
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+        Paint selectedPaint = new Paint(); selectedPaint.setColor(Color.RED); selectedPaint.setStyle(Paint.Style.STROKE); selectedPaint.setStrokeWidth(scaleCanvasDensity * 4);
+        Paint nonSelectedPaint = new Paint(selectedPaint); nonSelectedPaint.setColor(Color.GREEN);
+        android.graphics.Rect drawRectangleLeft = makeGraphicsRect(rectLeft,  scaleBmpPxToCanvasPx);
+        android.graphics.Rect drawRectangleMiddle = makeGraphicsRect(rectMiddle,  scaleBmpPxToCanvasPx);
+        android.graphics.Rect drawRectangleRight = makeGraphicsRect(rectRight,  scaleBmpPxToCanvasPx);
+        selection = (Selected) userContext; switch (selection) {
+            case LEFT:
+                canvas.drawRect(drawRectangleLeft, selectedPaint);
+                canvas.drawRect(drawRectangleMiddle, nonSelectedPaint);
+                canvas.drawRect(drawRectangleRight, nonSelectedPaint);
         Scalar lowerHsv = lowerRed;
         Scalar upperHsv = upperRed;
         switch (_alliance){
