d88fb318f79cd2568fa132dba4eeb140c3b728e1
asbeast <144757596+asbeast@users.noreply.github.com>
2023-11-25 16:19:23 -0800

    roadrunner install

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/FirstVisionProcessorV0.java
@@ -0,0 +1,137 @@
+package org.firstinspires.ftc.teamcode.processors;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.teamcode.common.Alliance;
+import org.firstinspires.ftc.teamcode.common.ScoringElementLocation;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class FirstVisionProcessorV0 implements VisionProcessor {
+    ScoringElementLocation selection = ScoringElementLocation.UNKNOWN;
+
+    // Define lower and upper HSV values for the red and blue color ranges
+    public Scalar lowerRed = new Scalar(0, 50, 50);
+    public Scalar upperRed = new Scalar(50, 255, 255);
+    public Scalar lowerBlue = new Scalar(100, 50, 50);
+    public Scalar upperBlue = new Scalar(140, 255, 255);
+
+    private Mat hsvImage;
+    private Mat mask;
+
+
+    int width = 640;
+    int height = 480;
+
+    // Divide the mask into three vertical regions
+    Rect leftRegion = new Rect(0, 0, width / 5, height);
+    Rect centerRegion = new Rect(width / 5, 0, 3 * (width / 5), height);
+    Rect rightRegion = new Rect(4 * (width / 5), 0, width / 5, height);
+
+    String telemetry = "";
+
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+    }
+
+
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+        telemetry = "";
+        // Convert the image to HSV format
+        Mat hsvImage = new Mat();
+        Imgproc.cvtColor(frame, hsvImage, Imgproc.COLOR_RGB2HSV);
+
+        // Define lower and upper HSV values for the red color range
+        Scalar lowerRed = new Scalar(0, 100, 100);
+        Scalar upperRed = new Scalar(10 , 255, 255);
+
+        // Create a mask to extract red-colored pixels
+//        Mat redMask = new Mat();
+        Core.inRange(hsvImage, lowerRed, upperRed, frame);
+
+//        Scalar lowerRed2 = new Scalar(150, 100, 100);
+//        Scalar upperRed2 = new Scalar(180, 255, 255);
+//        // Create a mask to extract red-colored pixels
+//        Mat redMask2 = new Mat();
+//        Core.inRange(hsvImage, lowerRed2, upperRed2, redMask2);
+//        Core.add(redMask, redMask2, redMask);
+
+        // Get the dimensions of the image
+        int height = frame.rows();
+        int width = frame.cols();
+
+
+        Mat leftMask = new Mat(frame, leftRegion);
+        Mat centerMask = new Mat(frame, centerRegion);
+        Mat rightMask = new Mat(frame, rightRegion);
+
+        // Count the red pixels in each region
+        int leftCount = Core.countNonZero(leftMask);
+        int centerCount = Core.countNonZero(centerMask);
+        int rightCount = Core.countNonZero(rightMask);
+
+        telemetry += "leftCount = " + leftCount;
+        telemetry += " centerCount =" + centerCount;
+        telemetry += " rightCount = " + rightCount;
+
+        if (leftCount > centerCount && leftCount > rightCount){
+            selection =  ScoringElementLocation.LEFT;
+        } else if (centerCount > leftCount && centerCount > rightCount){
+            selection =  ScoringElementLocation.CENTER;
+        } else {
+            selection = ScoringElementLocation.RIGHT;
+        }
+        telemetry += " Selected " + selection.toString();
+
+//        frame = redMask.clone();
+//        redMask.CopyTo(frame);
+//        Core.bitwise_and(frame, frame, frame, redMask);
+
+        return telemetry;
+    }
+
+
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+        Paint selectedPaint = new Paint();
+        selectedPaint.setColor(Color.RED);
+        selectedPaint.setStyle(Paint.Style.STROKE);
+        selectedPaint.setStrokeWidth(scaleCanvasDensity * 4);
+        Paint nonSelectedPaint = new Paint(selectedPaint);
+        nonSelectedPaint.setColor(Color.GREEN);
+        android.graphics.Rect drawRectangleLeft = makeGraphicsRect(leftRegion, scaleBmpPxToCanvasPx);
+        android.graphics.Rect drawRectangleMiddle = makeGraphicsRect(centerRegion, scaleBmpPxToCanvasPx);
+        android.graphics.Rect drawRectangleRight = makeGraphicsRect(rightRegion, scaleBmpPxToCanvasPx);
+        canvas.drawRect(drawRectangleLeft, nonSelectedPaint);
+        canvas.drawRect(drawRectangleMiddle, nonSelectedPaint);
+        canvas.drawRect(drawRectangleRight, nonSelectedPaint);
+    }
+
+    public ScoringElementLocation getSelection() { return selection;
+    }
+
+    public void SetAlliance(Alliance alliance){
+        _alliance = alliance;
+    }
+
+    public String getTelemetry(){ return telemetry;}
+    private Alliance _alliance = Alliance.UNKNOWN;
+
+
+    private android.graphics.Rect makeGraphicsRect(Rect rect, float  scaleBmpPxToCanvasPx) {
+        int left = Math.round(rect.x * scaleBmpPxToCanvasPx);
+        int top = Math.round(rect.y * scaleBmpPxToCanvasPx);
+        int right = left + Math.round(rect.width * scaleBmpPxToCanvasPx); int bottom = top + Math.round(rect.height * scaleBmpPxToCanvasPx);
+        return new android.graphics.Rect(left, top, right, bottom); }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/FirstVisionOpmode.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.processors.FirstVisionProcessor;
+import org.firstinspires.ftc.teamcode.processors.FirstVisionProcessorV0;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import java.util.concurrent.TimeUnit;
+
+@Autonomous()
+public class FirstVisionOpmode extends OpMode {
+    private FirstVisionProcessor visionProcessor;
+    private VisionPortal visionPortal;
+
+    /**
+     * User-defined init method
+     * <p>
+     * This method will be called once, when the INIT button is pressed.
+     */
+    @Override
+    public void init() {
+        visionProcessor = new FirstVisionProcessor();
+        visionPortal = VisionPortal.easyCreateWithDefaults(
+                hardwareMap.get(WebcamName.class, "Webcam 1"), visionProcessor);
+
+//        setManualExposure(20, 60);
+    }
+
+    public void start(){
+        visionPortal.stopStreaming();
+    }
+
+    /**
+     * User-defined loop method
+     * <p>
+     * This method will be called repeatedly during the period between when
+     * the play button is pressed and when the OpMode is stopped.
+     */
+    @Override
+    public void loop() {
+        telemetry.addData("Identified", visionProcessor.getSelection().toString());
+    }
+
+    /*
+ Manually set the camera gain and exposure.
+ This can only be called AFTER calling initAprilTag(), and only works for Webcams;
+*/
+    private void    setManualExposure(int exposureMS, int gain) {
+        // Wait for the camera to be open, then use the controls
+        try {
+
+        if (visionPortal == null) {
+            return;
+        }
+
+        // Make sure camera is streaming before we try to set the exposure controls
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while ((visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                    Thread.sleep(20);
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+
+        // Set camera controls unless we are stopping.
+        {
+            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                exposureControl.setMode(ExposureControl.Mode.Manual);
+                Thread.sleep(50);
+            }
+            exposureControl.setExposure((long)exposureMS, TimeUnit.MILLISECONDS);
+            Thread.sleep(20);
+            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+            gainControl.setGain(gain);
+            Thread.sleep(20);
+        }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
