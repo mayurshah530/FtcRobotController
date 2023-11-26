12863f733cbec33483ee4cdd8606c30f7017228a
asbeast <144757596+asbeast@users.noreply.github.com>
2023-11-25 16:19:23 -0800

    roadrunner install

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevBotAuto.java
@@ -0,0 +1,359 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.common.ScoringElementLocation;
+import org.firstinspires.ftc.teamcode.common.Utils;
+import org.firstinspires.ftc.teamcode.mechanisms.RevStarterRobotHardware;
+import org.firstinspires.ftc.teamcode.processors.FirstVisionProcessor;
+import org.firstinspires.ftc.teamcode.processors.FirstVisionProcessorV0;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+import org.firstinspires.ftc.teamcode.common.Alliance;
+import org.firstinspires.ftc.teamcode.common.FieldPosition;
+
+/*
+ * RevBot Autonomous code
+ */
+@Autonomous(name = "RevBot Autonomous", group = "Auto")
+public class RevBotAuto extends OpMode {
+
+  RevStarterRobotHardware robot = new RevStarterRobotHardware(this);
+
+  private ElapsedTime runtime = new ElapsedTime();
+  private FirstVisionProcessorV0 visionProcessor;
+  private AprilTagProcessor aprilTag;
+
+  // Used to hold the data for a detected AprilTag
+  private AprilTagDetection desiredTag = null;
+
+  private VisionPortal visionPortal;
+
+  private ScoringElementLocation selectedSide = ScoringElementLocation.UNKNOWN;
+
+  private State state = State.START;
+
+  private double lastTime;
+
+  private Alliance alliance = Alliance.UNKNOWN;
+
+  private FieldPosition fieldPosition = FieldPosition.UNKNOWN;
+
+  private int desiredTagId;
+
+  private boolean aprilTagTargetFound = false;
+
+  final double DESIRED_DISTANCE = 12.0; //  this is how close the camera should get to the target (inches)
+  //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
+  //  applied to the drive motors to correct the error.
+  //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
+  final double SPEED_GAIN =   0.02 ;   //  Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+  final double TURN_GAIN  =   0.01 ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+  final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+  final double MAX_AUTO_TURN  = 0.25;  //  Clip the turn speed to this max value (adjust for your robot)
+
+
+  /// State machine
+  enum State {
+    // Initialize
+    START,
+    // Determine alliance and near/far side using gamepad entry
+    DETERMINE_ALLIANCE,
+    // Identify if the custom game element is placed on left/center/right using computer vision
+    IDENTIFY_SPIKE,
+    // Drive the robot to drag the pixel with it to be placed on the correct spike mark (red/blue tape)
+    PLACE_PIXEL_ON_SPIKE,
+    // Move the robot towards backdrop (for scoring)
+    MOVE_TOWARDS_BACKDROP,
+    //
+            n    ,
+    //
+    PARK,
+    //
+    END
+  }
+  /**
+   * This method will be called once, when the INIT button is pressed.
+   */
+  @Override
+  public void init() {
+    robot.init();
+    state = State.DETERMINE_ALLIANCE;
+    initVisionPortal();
+    telemetry.addData("Status", "Initialized");
+  }
+
+  /**
+   * This method will be called repeatedly during the period between when
+   * the init button is pressed and when the play button is pressed (or the
+   * OpMode is stopped).
+   */
+  @Override
+  public void init_loop() {
+    // Step 1: Determine blue vs. red alliance
+    if (alliance == Alliance.UNKNOWN){
+      alliance = Utils.GetAllianceFromKeyEntry(gamepad1);
+    }
+    telemetry.addData("Alliance", alliance.toString());
+
+    // Step 2: Determine near or far position from the scoring board
+    if (fieldPosition == FieldPosition.UNKNOWN){
+      fieldPosition = Utils.GetFieldPositionFromKeyEntry(gamepad1);
+    }
+    telemetry.addData( "FieldPosition" , fieldPosition.toString());
+
+    // Check if we know both alliance and field position
+    if (alliance != Alliance.UNKNOWN && fieldPosition != FieldPosition.UNKNOWN) {
+      state = State.IDENTIFY_SPIKE;
+    }
+  }
+
+  /**
+   * This method will be called once, when the play button is pressed.
+   */
+  @Override
+  public void start() {
+
+    runtime.reset();
+    lastTime = getRuntime();
+    if (state == State.IDENTIFY_SPIKE){
+      visionProcessor.SetAlliance(alliance);
+    }
+  }
+
+  /**
+   * This method will be called repeatedly during the period between when
+   * the play button is pressed and when the OpMode is stopped.
+   */
+  @Override
+  public void loop() {
+
+    switch (state) {
+      case START:
+      case DETERMINE_ALLIANCE:
+        telemetry.addData("Unexpected State: " , state.toString());
+        break;
+      case IDENTIFY_SPIKE: {
+        selectedSide = visionProcessor.getSelection();
+        if (selectedSide == ScoringElementLocation.UNKNOWN) {
+          break;
+        }
+        visionPortal.stopStreaming();
+        visionPortal.setProcessorEnabled(visionProcessor, false);
+        telemetry.addData("selectedSide: ", selectedSide.toString());
+        state = State.PLACE_PIXEL_ON_SPIKE;
+        lastTime = getRuntime();
+      }
+        break;
+      case PLACE_PIXEL_ON_SPIKE: {
+        telemetry.addData("selectedSide: ", selectedSide.toString());
+
+        // TODO: Depending on selectedSide, move left, straight, right.
+        //  Since we are dragging the pixel there is nothing to drop.
+        boolean targetReached = false;
+        if (selectedSide == ScoringElementLocation.LEFT) {
+          targetReached = driveToLeftSpikeMark();
+        } else if (selectedSide == ScoringElementLocation.CENTER) {
+          targetReached = driveToCenterSpikeMark();
+        } else {
+          targetReached = driveToRightSpikeMark();
+        }
+
+        if (!targetReached) {
+          break;
+        }
+
+        //  Move back a little so that we don't drag the pixel away from the desired location.
+
+
+        state = State.MOVE_TOWARDS_BACKDROP;
+        lastTime = getRuntime();
+      }
+
+        break;
+      case MOVE_TOWARDS_BACKDROP: {
+        desiredTagId = Utils.GetDesiredTagId(alliance, selectedSide);
+        telemetry.addData("desiredTagId ", desiredTagId);
+        visionPortal.setProcessorEnabled(aprilTag, true);
+        // TODO: Step 1: Move based on approximation - time/ encoder / gyro.
+        // Determine which direction to turn. Plan trajectory based on initial location and the goal location.
+        // Keep looking for the desired aprilTag
+        boolean moveSuccess = false;
+        if (!aprilTagTargetFound){
+          moveSuccess = moveTowardsBackdrop(alliance, selectedSide);
+        }
+        aprilTagTargetFound = findDesiredAprilTag(desiredTagId);
+
+        // Step 2: Move based on AprilTag location once it is found.
+        if (!aprilTagTargetFound) {
+          break;
+        }
+        boolean targetReached = driveToAprilTagTarget(desiredTag);
+        if (!targetReached) {
+          break;
+        }
+        state = State.SCORE_PIXEL;
+        lastTime = getRuntime();
+
+        visionPortal.setProcessorEnabled(aprilTag, false);
+      }
+        break;
+      case SCORE_PIXEL: {
+        // TODO: raise arm / wrist to the scoring position, open gripper
+        state = State.PARK;
+        lastTime = getRuntime();
+      }
+        break;
+      case PARK: {
+        // If we started on the near side, move towards the corner.
+        // If we started on the far side, move to towards the center
+        state = State.END;
+        lastTime = getRuntime();
+      }
+      break;
+      case END:
+        break;
+      default:
+        telemetry.addData("Invalid State: " , state.toString());
+        break;
+    }
+
+    telemetry.addData("State: " , state.toString());
+    telemetry.addData("Runtime", getRuntime());
+    telemetry.addData("Time in State", getRuntime() - lastTime);
+  }
+
+  /**
+   * This method will be called once, when this OpMode is stopped.
+   * <p>
+   * Your ability to control hardware from this method will be limited.
+   */
+  @Override
+  public void stop() {
+  }
+
+  /**
+   * Init vision portal. AprilTag and VisionProcessor
+   */
+  private void initVisionPortal() {
+    visionProcessor = new FirstVisionProcessorV0();
+    // Create the AprilTag processor by using a builder.
+    aprilTag = new AprilTagProcessor.Builder().build();
+    aprilTag.setDecimation(1);
+    visionPortal = new VisionPortal.Builder()
+            .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+            .addProcessors(visionProcessor, aprilTag)
+            .build();
+    // At the beginning, vision processor on and aprilTag processor off
+    visionPortal.setProcessorEnabled(aprilTag, false);
+    visionPortal.setProcessorEnabled(visionProcessor, true);
+  }
+
+  boolean findDesiredAprilTag(int desiredTagId){
+    boolean targetFound = false;
+    desiredTag  = null;
+
+    // Step through the list of detected tags and look for a matching tag
+    List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+    for (AprilTagDetection detection : currentDetections) {
+      // Look to see if we have size info on this tag.
+      if (detection.metadata != null) {
+        //  Check to see if we want to track towards this tag.
+        if ((desiredTagId < 0) || (detection.id == desiredTagId)) {
+          // Yes, we want to use this tag.
+          targetFound = true;
+          desiredTag = detection;
+          break;  // don't look any further.
+        } else {
+          // This tag is in the library, but we do not want to track it right now.
+          telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+        }
+      } else {
+        // This tag is NOT in the library, so we don't have enough information to track to it.
+        telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+      }
+    }
+    if (targetFound) {
+      telemetry.addData("Found", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
+      telemetry.addData("Range",  "%5.1f inches", desiredTag.ftcPose.range);
+      telemetry.addData("Bearing","%3.0f degrees", desiredTag.ftcPose.bearing);
+    }
+    return targetFound;
+  }
+
+  boolean driveToAprilTagTarget(AprilTagDetection desiredTag){
+    if (desiredTag == null){
+      return false;
+    }
+
+    // Determine heading and range error so we can use them to control the robot automatically.
+    double  rangeError   = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+    double  headingError = desiredTag.ftcPose.bearing;
+
+    // Use the speed and turn "gains" to calculate how we want the robot to move.  Clip it to the maximum
+    double drive = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+    double turn  = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
+
+    telemetry.addData("Auto","Drive %5.2f, Turn %5.2f", drive, turn);
+    telemetry.addData("Auto","Range Err %5.2f, Heading Err %5.2f", rangeError, headingError);
+    robot.moveRobot(drive, turn);
+
+    // TODO: Tune this
+    if (rangeError < 2) {
+      return true;
+    }
+
+    return false;
+  }
+
+
+  // TODO: Implement this
+  private boolean moveTowardsBackdrop(Alliance alliance, ScoringElementLocation selectedSide) {
+
+    return false;
+  }
+
+  // TODO: Implement this
+  private boolean moveBlueNearToBackdrop(){
+    return false;
+  }
+
+  // TODO: Implement this
+  private boolean moveBlueFarToBackdrop(){
+    return false;
+  }
+
+  // TODO: Implement this
+  private boolean moveRedNearToBackdrop(){
+    return false;
+  }
+
+  // TODO: Implement this
+  private boolean moveRedFarToBackdrop(){
+    return false;
+  }
+
+  // TODO: Implement this
+  private boolean driveToLeftSpikeMark(){
+    return false;
+  }
+
+  // TODO: Implement this
+  private boolean driveToCenterSpikeMark(){
+    return false;
+  }
+
+  // TODO: Implement this
+  private boolean driveToRightSpikeMark(){
+    return false;
+  }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/FirstVisionProcessor.java
@@ -0,0 +1,101 @@
+package org.firstinspires.ftc.teamcode.processors;
+
+import android.graphics.Canvas;
+
+
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.teamcode.common.Alliance;
+import org.firstinspires.ftc.teamcode.common.ScoringElementLocation;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class FirstVisionProcessor implements VisionProcessor {
+    ScoringElementLocation selection = ScoringElementLocation.UNKNOWN;
+
+    // Define lower and upper HSV values for the red and blue color ranges
+    public Scalar lowerRed = new Scalar(0, 100, 100);
+    public Scalar upperRed = new Scalar(10, 255, 255);
+    public Scalar lowerBlue = new Scalar(100, 100, 100);
+    public Scalar upperBlue = new Scalar(140, 255, 255);
+
+    private Mat hsvImage;
+    private Mat mask;
+
+    // Divide the mask into three vertical regions
+    private Rect leftRegion;
+    private Rect centerRegion;
+    private Rect rightRegion;
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+        hsvImage = new Mat(height, width, CvType.CV_8UC3);
+        mask = new Mat(height, width, CvType.CV_8UC1);
+
+        // Divide the mask into three vertical regions
+        leftRegion = new Rect(0, 0, width / 5, height);
+        centerRegion = new Rect(width / 5, 0, 3 * (width / 5), height);
+        rightRegion = new Rect(4 * (width / 5), 0, width / 5, height);
+    }
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+        Scalar lowerHsv = lowerRed;
+        Scalar upperHsv = upperRed;
+        switch (_alliance){
+            case UNKNOWN:
+                return ScoringElementLocation.UNKNOWN;
+            case RED:
+                lowerHsv = lowerRed;
+                upperHsv = upperRed;
+                break;
+            case BLUE:
+                lowerHsv = lowerBlue;
+                upperHsv = upperBlue;
+                break;
+        }
+
+        // Convert the image to HSV format
+        Imgproc.cvtColor(frame, hsvImage, Imgproc.COLOR_RGB2HSV);
+
+        // Create a mask to extract red-colored pixels
+        Core.inRange(hsvImage, lowerHsv, upperHsv, mask);
+
+        Mat leftMask = new Mat(mask, leftRegion);
+        Mat centerMask = new Mat(mask, centerRegion);
+        Mat rightMask = new Mat(mask, rightRegion);
+
+        // Count the red pixels in each region
+        int leftCount = Core.countNonZero(leftMask);
+        int centerCount = Core.countNonZero(centerMask);
+        int rightCount = Core.countNonZero(rightMask);
+
+        if (leftCount > centerCount && leftCount > rightCount){
+            selection =  ScoringElementLocation.LEFT;
+        } else if (centerCount > leftCount && centerCount > rightCount){
+            selection =  ScoringElementLocation.CENTER;
+        } else {
+            selection = ScoringElementLocation.RIGHT;
+        }
+        return selection;
+    }
+
+
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+    }
+
+    public ScoringElementLocation getSelection() { return selection;
+    }
+
+    public void SetAlliance(Alliance alliance){
+        _alliance = alliance;
+    }
+
+
+    private Alliance _alliance = Alliance.BLUE;
+
+}
