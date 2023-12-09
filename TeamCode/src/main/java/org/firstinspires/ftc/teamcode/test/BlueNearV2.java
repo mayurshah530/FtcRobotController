5472b3b0cf00001b48ab473c4c70453673729061
asbeast <144757596+asbeast@users.noreply.github.com>
2023-12-09 01:07:27 -0800

    Update BlueNearV2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/BlueNearV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/BlueNearV2.java
@@ -15,7 +15,13 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.common.Alliance;
+import org.firstinspires.ftc.teamcode.common.ScoringElementLocation;
+import org.firstinspires.ftc.teamcode.processors.FirstVisionProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 @Config
 @Autonomous(name = "Blue Near V2", group = "RoadRunner 1.0")
@@ -31,12 +37,18 @@ public class BlueNearV2 extends LinearOpMode {
     Pose2d BLUE_NEAR_CENTER_SPIKE = new Pose2d(12, 24.5+HALF_ROBO_LEN, -Math.PI/2.0);
     Pose2d BLUE_BOARD_CENTER_TAG = new Pose2d(58 - HALF_ROBO_LEN, 36, 0);
 
+    private FirstVisionProcessor visionProcessor;
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
+    private ScoringElementLocation selectedSide = ScoringElementLocation.UNKNOWN;
+
     @Override
     public void runOpMode() {
 
         MecanumDrive drive = new MecanumDrive(hardwareMap, BLUE_NEAR_START_POSE);
 
-
+        initVisionPortal();
+        visionProcessor.SetAlliance(Alliance.BLUE);
 
         // 1. Start position: blue near; Game Element location: Center.
         // Declare trajectory
@@ -58,12 +70,42 @@ public class BlueNearV2 extends LinearOpMode {
 
         waitForStart();
 
+        selectedSide = visionProcessor.getSelection();
+        visionPortal.stopStreaming();
+        visionPortal.setProcessorEnabled(visionProcessor, false);
+        telemetry.addData("selectedSide: ", selectedSide.toString());
+        telemetry.update();
+
         if (isStopRequested()) return;
+
+        Action trajectoryToRun = TrajectoryBlueNearGeCenter;
+        switch (selectedSide){
+            case LEFT:
+                // TODO: Define left trajectory
+                trajectoryToRun = TrajectoryBlueNearGeCenter;
+                break;
+            case CENTER:
+                trajectoryToRun = TrajectoryBlueNearGeCenter;
+                break;
+            case RIGHT:
+                // TODO: Define left trajectory
+                trajectoryToRun = TrajectoryBlueNearGeCenter;
+                break;
+        }
+
+        // General plan
+        // Identify game element location (left/center/right)
+        // Go place pixel
+        // [later] Go towards scoring position
+        // [later] Enable AprilTag processor to find absolute location
+        // [later] Move to precise scoring position
+        // [later] Score
+        // Park
 
         Actions.runBlocking(
                 new SequentialAction(
-                        TrajectoryBlueNearGeCenter, // Example of a drive action
-                        // Only that this action uses a Lambda expression to reduce complexity
+                        trajectoryToRun, 
+                        // This action uses a Lambda expression to reduce complexity
                         (telemetryPacket) -> {
                             telemetry.addData("x", drive.pose.position.x);
                             telemetry.addData("y", drive.pose.position.y);
@@ -73,6 +115,20 @@ public class BlueNearV2 extends LinearOpMode {
                         }
                 )
         );
+    }
+
+    private void initVisionPortal() {
+        visionProcessor = new FirstVisionProcessor();
+        // Create the AprilTag processor by using a builder.
+        aprilTag = new AprilTagProcessor.Builder().build();
+        aprilTag.setDecimation(1);
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessors(visionProcessor, aprilTag)
+                .build();
+        // At the beginning, vision processor on and aprilTag processor off
+        visionPortal.setProcessorEnabled(aprilTag, false);
+        visionPortal.setProcessorEnabled(visionProcessor, true);
     }
 
 }
