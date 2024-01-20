588274a1816e3b44ec124b18bc8c546bd3fa29b5
asbeast <144757596+asbeast@users.noreply.github.com>
2024-01-20 00:30:12 -0800

    disable v2 auto code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RedFarV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RedFarV2.java
@@ -8,6 +8,7 @@ import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -22,6 +23,7 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 @Config
 @Autonomous(name = "Red Far V2", group = "RoadRunner 1.0")
+@Disabled
 public class RedFarV2 extends LinearOpMode {
 
     double HALF_ROBO_LEN = 9;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RedNearV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/RedNearV2.java
@@ -8,12 +8,14 @@ import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
 @Config
 @Autonomous(name = "Red Near V2", group = "RoadRunner 1.0")
+@Disabled
 public class RedNearV2 extends LinearOpMode {
 
     public static double HALF_ROBO_LEN = 9;
