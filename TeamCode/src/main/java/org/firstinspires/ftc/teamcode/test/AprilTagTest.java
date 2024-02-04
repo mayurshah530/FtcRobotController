a271a70f2a94dbaa2948feabea887e40046ed7a3
mayurshah530 <30601517+mayurshah530@users.noreply.github.com>
2024-02-03 17:40:34 -0800

    Bunch of changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/AprilTagTest.java
@@ -0,0 +1,158 @@
+package org.firstinspires.ftc.teamcode.test;
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.common.Alliance;
+import org.firstinspires.ftc.teamcode.common.ScoringElementLocation;
+import org.firstinspires.ftc.teamcode.common.Utils;
+import org.firstinspires.ftc.teamcode.mechanisms.Outtake;
+import org.firstinspires.ftc.teamcode.processors.FirstVisionProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+@Config
+@Autonomous(name = "April Tag Motion Test", group = "RoadRunner 1.0")
+public class AprilTagTest extends LinearOpMode {
+
+    public static boolean ENABLE_APRIL_TAG_CORRECTION = true;
+
+    public static double HALF_ROBO_LEN = 9;
+    public static double RED_NEAR_LEFT_SPIKE_X = 10;
+    public static double RED_NEAR_LEFT_SPIKE_Y = -30;
+
+
+    // Start position red far
+
+    // START POSITIONS
+    Pose2d RED_NEAR_START_POSE = new Pose2d(12, -(72-HALF_ROBO_LEN), Math.PI/2.0);
+
+
+    // PARK POSITIONS
+    Pose2d RED_RIGHT_PARK = new Pose2d(58 - HALF_ROBO_LEN, -66, 0);
+
+    // SPIKE Locations
+    Pose2d RED_NEAR_CENTER_SPIKE = new Pose2d(12, -(24.5+HALF_ROBO_LEN), Math.PI/2.0);
+    public static double RED_NEAR_RIGHT_SPIKE_X = 23.5-HALF_ROBO_LEN;
+    public static double RED_NEAR_RIGHT_SPIKE_Y = -25;
+    public static double RED_NEAR_RIGHT_SPIKE_BACK_X = 4;
+
+    Pose2d RED_NEAR_RIGHT_SPIKE = new Pose2d(RED_NEAR_RIGHT_SPIKE_X, RED_NEAR_RIGHT_SPIKE_Y, 0);
+    Pose2d RED_NEAR_LEFT_SPIKE = new Pose2d(RED_NEAR_LEFT_SPIKE_X, RED_NEAR_LEFT_SPIKE_Y, -Math.PI);
+
+    Pose2d RED_FAR_CENTER_SPIKE = new Pose2d(-36, -(24.5+HALF_ROBO_LEN), -Math.PI/2.0);
+    Pose2d RED_FAR_RIGHT_SPIKE = new Pose2d(23.5-HALF_ROBO_LEN, -(30), 0);
+    Pose2d RED_FAR_LEFT_SPIKE = new Pose2d(9, -(19.5)-HALF_ROBO_LEN, -(30));
+
+
+    Pose2d BLUE_NEAR_CENTER_SPIKE = new Pose2d(12, 24.5+HALF_ROBO_LEN, -Math.PI/2.0);
+    Pose2d BLUE_NEAR_RIGHT_SPIKE = new Pose2d(0.5+HALF_ROBO_LEN, 30+HALF_ROBO_LEN, -Math.PI);
+
+    // TAG locations
+    // TAG locations
+    public static double TAG_BOT_OFFSET = 20.25;
+    Pose2d RED_ALLIANCE_LEFT_TAG = new Pose2d(60.25 - TAG_BOT_OFFSET, -29.41, 0);
+    Pose2d RED_ALLIANCE_CENTER_TAG = new Pose2d(60.25 - TAG_BOT_OFFSET, -35.41, 0);
+    Pose2d RED_ALLIANCE_RIGHT_TAG = new Pose2d(60.25 - TAG_BOT_OFFSET, -41.41, 0);
+    // wait location
+
+
+    private FirstVisionProcessor visionProcessor;
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
+    private ScoringElementLocation selectedSide = ScoringElementLocation.CENTER;
+
+    private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
+    private final ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, RED_NEAR_START_POSE);
+        Outtake outtake = new Outtake(hardwareMap);
+
+        initVisionPortal();
+        visionProcessor.SetAlliance(Alliance.RED);
+
+        Action trajectoryActionCloseOut = drive.actionBuilder(drive.pose)
+                .strafeTo(new Vector2d(48, 12))
+                .build();
+        // Set to true when an AprilTag target is detected
+        boolean targetFound = false;
+
+        while(!isStopRequested() && !opModeIsActive()) {
+
+        }
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        int desiredTagId = Utils.GetDesiredTagId(Alliance.RED, selectedSide);
+        visionPortal.setProcessorEnabled(aprilTag, true);
+        sleep(100);
+
+        if(ENABLE_APRIL_TAG_CORRECTION){
+            boolean aprilTagConverged = false;
+            runtime.reset();
+            while(runtime.seconds() < 10.0 && !aprilTagConverged) {
+                desiredTag = null;
+                // Step through the list of detected tags and look for a matching tag
+                List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+                for (AprilTagDetection detection : currentDetections) {
+                    if ((detection.metadata != null) && (detection.id == desiredTagId)) {
+                        targetFound = true;
+                        desiredTag = detection;
+                        break;  // don't look any further.
+                    } else {
+                        telemetry.addData("Unknown Target", "Tag ID %d is not in TagLibrary\n", detection.id);
+                    }
+                }
+
+                if (targetFound) {
+                    telemetry.addData("Target", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
+                    telemetry.addData("x", "%5.1f inches", desiredTag.ftcPose.x);
+                    telemetry.addData("y", "%5.1f inches", desiredTag.ftcPose.y);
+                    telemetry.addData("Yaw", "%3.0f degrees", desiredTag.ftcPose.yaw);
+                    telemetry.addData("Range", "%5.1f inches", desiredTag.ftcPose.range);
+                    telemetry.addData("Bearing", "%3.0f degrees", desiredTag.ftcPose.bearing);
+                    telemetry.addData("Robot Pose", "%3.1f, %3.1 %3.1f", drive.pose.position.x, drive.pose.position.y, drive.pose.heading.log());
+                    telemetry.update();
+
+                    aprilTagConverged = drive.alignToAprilTag(desiredTag);
+                }
+            }
+        }
+
+    } // runOpMode
+
+
+    private void initVisionPortal() {
+        visionProcessor = new FirstVisionProcessor();
+        // Create the AprilTag processor by using a builder.
+        aprilTag = new AprilTagProcessor.Builder().build();
+        aprilTag.setDecimation(1);
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessors(visionProcessor, aprilTag)
+                .build();
+        // At the beginning, vision processor on and aprilTag processor off
+        visionPortal.setProcessorEnabled(aprilTag, false);
+        visionPortal.setProcessorEnabled(visionProcessor, true);
+    }
+
+}
